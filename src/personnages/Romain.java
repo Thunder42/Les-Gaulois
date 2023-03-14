39553ea2f80e60ab32d05365e2d24e5653edf099
Louis Dutheil <dtl4500a@ens.fsi>
2023-03-14 16:45:46 +0100

    Assertions romain

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -5,6 +5,7 @@ public class Romain {
 	private int force;
 	
 	public Romain(String nom, int force) {
+		assert (force > 0);
 		this.nom = nom;
 		this.force = force;
 	}
@@ -18,15 +19,18 @@ public class Romain {
 		return "Le romain " + nom + " : ";
 	}
 	public void recevoirCoup(int forceCoup) {
+		assert (force > 0);
+		int oldForce = force;
 		force-=forceCoup;
 		if (force>0) {
 			parler("Aïe!");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert (force > oldForce);
 	}
 	public static void main(String[] args) {
-		Romain baba = new Romain("Orum",4);
+		Romain baba = new Romain("Orum",6);
 		System.out.println(baba);
 		System.out.println(baba.prendreParole());
 		baba.parler("Vous connaissez ma soeur Yaga ?");
